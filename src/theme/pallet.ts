50e58030cf3265d2445f4ca66834c840f637e6e8
MaeNeung <maeneung@kakao.com>
2026-02-28 10:19:27 +0900

    theme 작동 방식 수정

--- src/theme/pallet.ts
+++ src/theme/pallet.ts
@@ -1,22 +1,18 @@
-interface Palette {
-  name: string;
-  value: string;
+export const fontPalette: Record<string, string| number> =
+    {
+      "mainText" : "18px",
+      "subText" : "12px",
+      "mainHeader" : "20px",
+    };
+
+export const colorPalette: Record<string, string| number> = {
+  "test1" : "#FFFFFF",
+  "test2" : "#FFFFFF",
+  "test3" : "#FFFFFF",
+  "test4" : "#FFFFFF",
+  "test5" : "#FFFFFF",
+  "test6" : "#FFFFFF",
+  "test7" : "#FFFFFF",
+  "test8" : "#FFFFFF",
+  "test9" : "#FFFFFF",
 }
-
-export const fontPalette: Palette[] = [
-  { name: "mainText", value: "18px" },
-  { name: "subText", value: "12px" },
-  { name: "mainHeader", value: "24px" },
-];
-
-export const colorPalette: Palette[] = [
-  { name: "test1", value: "#FFFFFF" },
-  { name: "test2", value: "#FFFFFF" },
-  { name: "test3", value: "#FFFFFF" },
-  { name: "test4", value: "#FFFFFF" },
-  { name: "test5", value: "#FFFFFF" },
-  { name: "test6", value: "#FFFFFF" },
-  { name: "test7", value: "#FFFFFF" },
-  { name: "test8", value: "#FFFFFF" },
-  { name: "test9", value: "#FFFFFF" },
-];
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,17 +2,11 @@ import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react";
 import { colorPalette, fontPalette } from "./pallet.ts";
 
 const config = () => {
-  const fonts: Record<string, string> = {};
-  const colors: Record<string, string> = {};
-
-  for (const font of fontPalette) fonts[font.name] = font.value;
-  for (const color of colorPalette) colors[color.name] = color.value;
-
   return defineConfig({
     theme: {
       tokens: {
-        fonts: fonts as never,
-        colors: colors as never,
+        fonts: fontPalette as never,
+        colors: colorPalette as never,
       },
     },
   });
