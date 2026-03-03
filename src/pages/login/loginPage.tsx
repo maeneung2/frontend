d09be027b5f7025b57524be1b82738924dca6beb
MaeNeung <maeneung@kakao.com>
2026-03-03 20:33:57 +0900

    add login logic

--- src/pages/login/loginPage.tsx
+++ src/pages/login/loginPage.tsx
@@ -1,13 +1,38 @@
 import { Link } from "react-router-dom";
-import { Flex } from "@chakra-ui/react";
+import { Form, Input } from "antd";
+import { Button, Flex } from "@chakra-ui/react";
+import axios from "axios";
 
 const LoginPage = () => {
   return (
-    <Flex flexDir={"column"}>
-      로그인 페이지
-      <Link to={"/login/find-password"}>비밀번호 찾기 페이지</Link>
-      <Link to={"/login/sign-up"}>회원가입 페이지</Link>
-      <Link to={"/"}>메인 페이지</Link>
+    <Flex flexDir={"column"} p={4}>
+      <Form
+        onFinish={async ({ id, password }) => {
+          const res = await axios.post("http://localhost:3000/api/v1/user/login", {
+            id,
+            password,
+          });
+          const token = res.data.accessToken;
+          console.log("token", token);
+        }}
+      >
+        로그인 페이지
+        <Link to={"/login/find-password"}>비밀번호 찾기 페이지</Link>
+        <Link to={"/login/sign-up"}>회원가입 페이지</Link>
+        <Form.Item noStyle name={"id"}>
+          <Flex flexDir={"column"}>
+            아이디
+            <Input />
+          </Flex>
+        </Form.Item>
+        <Form.Item noStyle name={"password"}>
+          <Flex flexDir={"column"}>
+            비밀번호
+            <Input type={"password"} />
+          </Flex>
+        </Form.Item>
+        <Button type={"submit"}>로그인</Button>
+      </Form>
     </Flex>
   );
 };
--- src/pages/login/signup/signupPage.tsx
+++ src/pages/login/signup/signupPage.tsx
@@ -1,11 +1,66 @@
-import { Link } from "react-router-dom";
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
+import { Form, Input } from "antd";
+import axios from "axios";
+import { useCallback } from "react";
 
 const SignupPage = () => {
+  const handleSignup = useCallback(
+    async ({
+      userName,
+      phone,
+      password,
+      passwordConfirm,
+    }: {
+      userName: string;
+      phone: string;
+      password: string;
+      passwordConfirm: string;
+    }) => {
+      if (password !== passwordConfirm) {
+        alert("비밀번호 불일치");
+        return;
+      }
+      const res = await axios.post("http://localhost:3000/api/v1/user", {
+        userName,
+        phone,
+        password,
+      });
+      const token = res.data.accessToken;
+      console.log("token", token);
+    },
+    []
+  );
+
   return (
-    <Flex flexDir={"column"}>
-      회원가입 페이지
-      <Link to={"/"}>메인 페이지</Link>
+    <Flex flexDir={"column"} p={4}>
+      <Form onFinish={handleSignup}>
+        회원가입 페이지
+        <Form.Item noStyle name={"userName"}>
+          <Flex flexDir={"column"}>
+            아이디
+            <Input />
+          </Flex>
+        </Form.Item>
+        <Form.Item noStyle name={"phone"}>
+          <Flex flexDir={"column"}>
+            핸드폰
+            <Input />
+          </Flex>
+        </Form.Item>
+        <Form.Item noStyle name={"password"}>
+          <Flex flexDir={"column"}>
+            비밀번호
+            <Input type={"password"} />
+          </Flex>
+        </Form.Item>
+        <Form.Item noStyle name={"passwordConfirm"}>
+          <Flex flexDir={"column"}>
+            비밀번호 확인
+            <Input type={"password"} />
+          </Flex>
+        </Form.Item>
+        <Button type={"submit"}>회원가입</Button>
+      </Form>
     </Flex>
   );
 };
